3f65c881111bf0808280d74e6ce09c5363b00829
Rafael Marcelo <rafael.2018.ads@gmail.com>
2021-11-01 11:20:26 -0300

    Feito os componentes estaticos basicos

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,13 @@
+const Footer = () => {
+    return (
+        <footer className="footer mt-auto py-3 bg-dark">
+            <div className="container">
+                <p className="text-light">App desenvolvido por <a href="https://github.com/rafaelmarcelo08" target="_blank" rel="noreferrer">Rafael Marcelo</a></p>
+                <p className="text-light"><small><b>Semana Spring React</b><br />
+                    Evento promovido pela escola DevSuperior: <a href="https://instagram.com/devsuperior.ig" target="_blank" rel="noreferrer">@devsuperior.ig</a></small></p>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
